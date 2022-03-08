2c368cd19d24ea2915afa266a2aeb707356b4b12
Anirudh Singh Chouhan <73285225+johnwick771981@users.noreply.github.com>
2022-03-09 00:46:12 +0530

    Create CoinFlip.sol

---
+++ CoinFlip.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract CoinFlip{
+
+    address public owner;
+    address[] public players;
+    
+    struct Player
+    {
+        uint256 balance;
+        address playerAddress; 
+    }
+
+    mapping (address => Player) public playerWallet;
+    
+    constructor()
+    {
+        owner = msg.sender;
+        playerWallet[owner] = Player(0,owner);        
+    }
+
+    function genRandom() public view returns(uint) {
+        return uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)))%2;
+    }
+
+    function flipCoin(uint256 bet) public payable returns (uint256)
+    {
+        uint256 randomValue = uint256(genRandom());
+        uint256 money = msg.value;
+        if (uint256(bet) == randomValue)
+        {
+            payWinner(msg.sender,money*2);  
+        }
+        else
+        {
+            playerWallet[msg.sender].balance -= money;
+            payWinner(owner,money);
+        }
+
+        return randomValue;
+    }
+
+    function depositMoney(address playerAddress) public payable returns (uint256) 
+    {
+        uint256 money =  msg.value;
+        
+        playerWallet[playerAddress].balance += money;
+        playerWallet[playerAddress].playerAddress = playerAddress;
+        
+        return playerWallet[playerAddress].balance;
+    }
+
+    function payWinner(address winnerAddress, uint256 money) public
+    {
+        uint256 amount = 1 ether; 
+        payable(winnerAddress).transfer(money*amount);
+        playerWallet[winnerAddress].balance += money;       
+    }    
+}
